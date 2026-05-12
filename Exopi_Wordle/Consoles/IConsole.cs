d582bc01a746f97f2f005acbd1dd038fe2bb64d2
Mikkel Østergaard <Mikkel stergaard@DESKTOP-0786LNF>
2026-05-12 10:09:24 +0200

    Add project files.

---
+++ Exopi_Wordle/Consoles/IConsole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.Consoles
+{
+    public interface IConsole
+    {
+        void Write(string message);
+        void WriteLine(string message);
+        string ReadLine();
+    }
+}
---
+++ Exopi_WordleTests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Exopi_WordleTests
+{
+    [TestClass]
+    public sealed class WordleTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
